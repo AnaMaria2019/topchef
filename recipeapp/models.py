4de9503d2c2e8f19b3d4db430b4569ccf49caea7
Ana <ana.maria.adina98@gmail.com>
2020-01-11 10:35:29 +0200

    Added category and tag models

--- recipeapp/models.py
+++ recipeapp/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 User = get_user_model()
 
+
 class UserProfile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     avatar = models.FileField(upload_to='media/', blank=True, null=True)
-    selfdescription = models.CharField(max_length=500)    
+    selfdescription = models.CharField(max_length=500)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
