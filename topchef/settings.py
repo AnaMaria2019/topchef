a1f3c6631d5c1cb3eae31fcd3e09b36d7a8e6612
Luea12 <cristina.badic12@gmail.com>
2020-01-11 10:02:51 +0200

    Fixed the setup

--- topchef/settings.py
+++ topchef/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipeapp.apps.RecipeappConfig'
+    'recipeapp'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/uploads/'
+
+LOGIN_URL = '/login/'
--- topchef/urls.py
+++ topchef/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import django.conf import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('recipeapp/', include('recipeapp.urls'))
     path('admin/', admin.site.urls),
-]
+    path('', include('recipeapp.urls')),
+] + (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
